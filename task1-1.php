666045397a860bb01dad442bf4c829c15d7792e8
hosono <pmrnansm@icloud.com>
2023-08-14 18:34:12 +0900

    task課題1

---
+++ task1-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  echo "<p>暑中見舞い申し上げます";
+  echo "<br>";
+  echo "暑中見舞い</p>"
+  echo "申し上げます</p>";
+  ?>
+</body>
+</html>
